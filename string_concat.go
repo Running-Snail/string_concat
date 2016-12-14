f7ea7f8e2056bc0410ba3ad39741fae3a1f67557
ZhihaoJun <zhao11fs@gmail.com>
2016-12-14 15:42:07 +0800

    add self concat benchmarks

--- string_concat.go
+++ string_concat.go
@@ -24,3 +24,27 @@ func joinFmt(a, b string) string {
 func joinStringsJoin(a, b string) string {
 	return strings.Join([]string{a, b}, "")
 }
+
+func selfConcatAdd(s string, n int) string {
+	ret := ""
+	for i := 0; i < n; i++ {
+		ret += s
+	}
+	return ret
+}
+
+func selfConcatBytes(s string, n int) string {
+	bf := bytes.NewBuffer([]byte{})
+	for i := 0; i < n; i++ {
+		bf.WriteString(s)
+	}
+	return bf.String()
+}
+
+func selfConcatJoin(s string, n int) string {
+	strs := make([]string, n)
+	for i := 0; i < n; i++ {
+		strs[i] = s
+	}
+	return strings.Join(strs, "")
+}
--- string_concat_test.go
+++ string_concat_test.go
@@ -45,3 +45,33 @@ func BenchmarkJoinStringJoin(b *testing.B) {
 		str3 = joinStringsJoin(str1, str2)
 	}
 }
+
+func BenchmarkSelfConcatAdd1000(b *testing.B) {
+	str1 := "haha"
+	ret := ""
+	_ = ret
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret = selfConcatAdd(str1, 1000)
+	}
+}
+
+func BenchmarkSelfConcatBytes1000(b *testing.B) {
+	str1 := "haha"
+	ret := ""
+	_ = ret
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret = selfConcatBytes(str1, 1000)
+	}
+}
+
+func BenchmarkSelfConcatJoin1000(b *testing.B) {
+	str1 := "haha"
+	ret := ""
+	_ = ret
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret = selfConcatJoin(str1, 1000)
+	}
+}
